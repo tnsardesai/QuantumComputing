8e9779ea07f74bb0d2a512424bfb77c27ab45022
thearnavgarg <thearnavgarg@gmail.com>
2019-05-19 14:46:03 -0700

    added classical for grover

---
+++ classical_grover.py
@@ -0,0 +1,30 @@
+import numpy as np
+import itertools
+
+def create_Zf(f, bits):
+    dim = 2**bits
+    # creating a 2^n x 2^n zeros matrix.
+    Zf = np.eye(dim, dtype=int)
+    # This creates a list of the different permutations of n bits.
+    lst_bitseq = list(map(list, itertools.product([0, 1], repeat=bits)))
+    for col, bitseq in enumerate(lst_bitseq):
+        if f(bitseq) == 1:
+            Zf[col, col] = -1
+            break
+    return Zf
+
+def create_Z0(bits):
+    dim = 2**bits
+    Z0 = np.eye(dim, dtype=int)
+    Z0[0,0] = -1
+    return Z0
+
+def num_iters(bits):
+    return np.floor((np.pi/4)*np.sqrt(2**bits))
+
+
+def main():
+    print(create_Z0(2))
+    print(create_Zf((lambda x: 1 if x == [1,0] else 0), 2))
+
+main()
