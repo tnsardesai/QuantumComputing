5be842435049f192c43950fd1df0395b561049de
thearnavgarg <thearnavgarg@gmail.com>
2019-05-28 19:29:41 -0700

    CODE COMPLETE

--- test.py
+++ test.py
@@ -1,15 +1,10 @@
-import numpy as np
-import itertools
 
-from pyquil import Program, get_qc
-from pyquil.gates import *
-from pyquil.quil import DefGate
+def testing():
+    yield 1
+    yield 2
 
-uf = [['a',0],[0,1]]
-# Get the Quil definition for the new gate
-uf_definition = DefGate("UF", uf)
-# Get the gate constructor
-UF = uf_definition.get_constructor()
+t = testing()
 
-p = Program()
-p += uf_definition
+for i in range(5):
+    print(next(t))
+
