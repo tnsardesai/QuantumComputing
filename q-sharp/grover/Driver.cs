028cc6693f14b6b2da1988c85f5aadfbbb1dd51a
Simranjit Singh <simranjit2112@gmail.com>
2019-06-08 23:49:21 -0700

    Grover Code: Initial

---
+++ q-sharp/grover/Driver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace Grover
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            using (var qsim = new QuantumSimulator())
+            {
+                var result = RunGroverSearch.Run(qsim).Result;
+                System.Console.Write(result);
+            }
+        }
+    }
+}
